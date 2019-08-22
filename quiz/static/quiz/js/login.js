617150a8299df6870db3d0e5c53194c75866af90
cdfom <m.folini@carbon-delta.com>
2019-08-22 17:52:17 +0200

    Fix small ui bug in login screen

--- quiz/static/quiz/js/login.js
+++ quiz/static/quiz/js/login.js
@@ -3,7 +3,11 @@
 var flipcard = document.getElementById('flipcard');
 var front = document.querySelector('.front');
 var back = document.querySelector('.back');
-flipcard.style.height = front.offsetHeight.toString() + 'px';
+if (flipcard.classList.contains('flipped')) {
+    flipcard.style.height = back.offsetHeight.toString() + 'px';
+} else {
+    flipcard.style.height = front.offsetHeight.toString() + 'px';
+}
 
 Array.from(document.querySelectorAll('.toggle-card')).forEach(function(el) {
     el.addEventListener('click', function(event) {
