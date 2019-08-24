c3e74663f159dbaed766bff49550eb51280b0416
laiskaSiili <marc.folini@gmail.com>
2019-08-25 00:32:33 +0200

    create dev and production settings

--- main/settings_production.py
+++ main/settings_production.py
@@ -1,11 +1,12 @@
 import os
 
+print('----------------')
+print('-- PRODUCTION --')
+print('----------------')
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-DEBUG = eval(os.environ.get('DEBUG'))
-
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 SECURE_CONTENT_TYPE_NOSNIFF= True
@@ -15,21 +16,17 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 
-# Application definition
-
 INSTALLED_APPS = [
-	'glp',
-	'django_cleanup',
-	'colorful',
-	'sortedm2m',
-	'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'quiz',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -41,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_project.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
@@ -55,18 +52,17 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-			'string_if_invalid':''
         },
     },
 ]
 
-WSGI_APPLICATION = 'django_project.wsgi.application'
+WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'scslch_database',
-        'USER': 'scslch_db',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USERNAME'),,
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '',
@@ -88,35 +84,26 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# INTERNATIONALIZATION
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-gettext = lambda s: s
-LANGUAGES = (
-	('de', gettext('German')),
-	('fr', gettext('French')),
-    ('en', gettext('English')),
-)
-
-LANGUAGE_CODE = 'de'
-
-TIME_ZONE = 'Europe/Zurich'
 
 # STATIC
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # EMAIL
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mfolini'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
+
+# QUIZ APP
+SPRINT_NR_QUESTIONS_PER_DIFFICULTY = 5
+NR_LEADERBOARD_ENTRIES_TO_SHOW = 5
+
+# crispy forms template pack
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
