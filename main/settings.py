293dfbcfb0205e06ae234f7c5076763fbb7f92df
laiskaSiili <marc.folini@gmail.com>
2019-08-25 20:16:26 +0200

    prototype with settings

--- main/settings.py
+++ main/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = eval(os.environ.get('DEBUG', 'False'))
 
-if eval(os.environ.get('NONDJANGO_DEBUG', 'False')) or DEBUG:
-    from .settings_development import *
-else:
+if eval(os.environ.get('LOAD_PROD_SETTINGS', 'True')):
     from .settings_production import *
+else:
+    from .settings_development import *
