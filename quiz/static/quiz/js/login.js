528f69421fe6c7d0449ff792e1abf6e99ae2664d
laiskaSiili <marc.folini@gmail.com>
2019-08-25 23:20:57 +0200

    fix flipcard ui for safar and webkit

--- quiz/static/quiz/js/login.js
+++ quiz/static/quiz/js/login.js
@@ -1,28 +1,31 @@
 "use strict";
 
-var flipcard = document.getElementById('flipcard');
-var front = document.querySelector('.front');
-var back = document.querySelector('.back');
-if (flipcard.classList.contains('flipped')) {
-    flipcard.style.height = back.offsetHeight.toString() + 'px';
-} else {
-    flipcard.style.height = front.offsetHeight.toString() + 'px';
-}
+$(document).ready(function() {
 
-Array.from(document.querySelectorAll('.toggle-card')).forEach(function(el) {
-    el.addEventListener('click', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        if (flipcard.classList.contains('flipped')) {
-            setTimeout(function(){
-                flipcard.style.height = front.offsetHeight.toString() + 'px';
-            }, 500);
-            flipcard.classList.remove('flipped');
-        } else {
-            setTimeout(function() {
-                flipcard.classList.add('flipped');
-            }, 500);
-            flipcard.style.height = back.offsetHeight.toString() + 'px';
-        }
-    }, true)
+    var flipcard = document.getElementById('flipcard');
+    var front = document.querySelector('.front');
+    var back = document.querySelector('.back');
+    if (flipcard.classList.contains('flipped')) {
+        flipcard.style.height = back.offsetHeight.toString() + 'px';
+    } else {
+        flipcard.style.height = front.offsetHeight.toString() + 'px';
+    }
+
+    Array.from(document.querySelectorAll('.toggle-card')).forEach(function(el) {
+        el.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            if (flipcard.classList.contains('flipped')) {
+                setTimeout(function(){
+                    flipcard.style.height = front.offsetHeight.toString() + 'px';
+                }, 500);
+                flipcard.classList.remove('flipped');
+            } else {
+                setTimeout(function() {
+                    flipcard.classList.add('flipped');
+                }, 500);
+                flipcard.style.height = back.offsetHeight.toString() + 'px';
+            }
+        }, true)
+    });
 });
