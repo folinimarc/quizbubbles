9c5fdfdfeabfc81db34fd9c8e559f832607cfebb
laiskaSiili <marc.folini@gmail.com>
2019-08-28 01:37:34 +0200

    remove spinner from login page

--- quiz/static/quiz/js/public_bubbles_list.js
+++ quiz/static/quiz/js/public_bubbles_list.js
@@ -8,12 +8,7 @@ $(document).ready(function() {
         stateSave: true,
         stateDuration: -1,
         "fnDrawCallback": function( oSettings ) {
-            // hide spinner when table is completely loaded
-            document.getElementById('main-container').classList.remove('opacity-zero');
-            document.getElementById('spinner').classList.add('opacity-zero');
-            setTimeout(function() {
-                document.getElementById('spinner').className = 'd-none';
-            }, 1000);
+            document.getElementById('table-container').classList.remove('opacity-zero');            
         },
     });
 });
