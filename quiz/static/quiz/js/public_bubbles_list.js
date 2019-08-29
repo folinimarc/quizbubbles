062d7b283532ec22a35a3a510e4b92aa48f6ed02
laiskaSiili <marc.folini@gmail.com>
2019-08-29 17:59:46 +0200

    replace filter label with placeholder

--- quiz/static/quiz/js/public_bubbles_list.js
+++ quiz/static/quiz/js/public_bubbles_list.js
@@ -10,6 +10,10 @@ $(document).ready(function() {
         stateDuration: -1,
         pageLength: 25,
         bLengthChange: false,
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search..."
+        },
         initComplete: function(settings, json) {
             document.getElementById('loading-container').classList.remove('opacity-zero');
         },
@@ -18,7 +22,7 @@ $(document).ready(function() {
             var pagination = $(this).closest('.dataTables_wrapper').find('.dataTables_paginate');
             pagination.toggle(this.api().page.info().pages > 1);
             // add classes to search and length elements to left align on mobile
-            $(this).closest('.dataTables_wrapper').find('.dataTables_filter').addClass('text-left');
+            $(this).closest('.dataTables_wrapper').find('.dataTables_filter').addClass('text-center text-sm-left');
             //$(this).closest('.dataTables_wrapper').find('.dataTables_length').addClass('text-left text-md-left');
             /*
             // hide filter
